b62e1804d44333ad97e9ae7e01201a71fda1f8ad
John Shaffer <john@insilica.co>
2023-09-29 16:32:15 -0500

    Download json zips from OpenFDA CDN

---
+++ stages/01_list.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# Script to download files
+
+# Get local path
+localpath=$(pwd)
+echo "Local path: $localpath"
+
+# Set list path
+listpath="$localpath"/list
+echo "List path: $listpath"
+
+# Create list path
+mkdir -p "$listpath"
+cd "$listpath"
+
+# Define the endpoint base address
+endpoint="https://api.fda.gov"
+
+# Retrieve the list of files to download from endpoint
+wget $endpoint/download.json
+
+echo "Download done."
