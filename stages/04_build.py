c9e69da4ba05c3b0396adfbec2ea61ccd7db27bf
bhlieberman <bhlieberman93@gmail.com>
2024-05-16 22:33:44 +0000

    parsing of CAS data from other_substances done

--- stages/04_build.py
+++ stages/04_build.py
@@ -1,5 +1,6 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from functools import cached_property
+from itertools import islice
 import json
 from json_flattener import flatten, GlobalConfig, KeyConfig
 import numpy as np
@@ -8,6 +9,7 @@ from pathlib import Path
 import pyarrow as pa
 import pyarrow.parquet as pq
 import os
+import toolz
 
 os.mkdir("brick")
 schemas_path = Path("schemas")
@@ -46,18 +48,27 @@ def read_json_data(paths):
 JSON_DATA = read_json_data(json_paths)
 
 
+def get_first_val(vs):
+    if len(vs) == 1:
+        return vs[0]
+    else:
+        return "|".join((val for val in vs))
+
+
 @dataclass(frozen=True)
 class DrugsFda:
     raw = JSON_DATA["drugs_fda"]
-    cols: dict = {
-        "openfda": [
-            "brand_name",
-            "application_number",
-            "generic_name",
-            "product_ndc",
-            "unii",
-        ]
-    }
+    cols: dict = field(
+        default={
+            "openfda": [
+                "brand_name",
+                "application_number",
+                "generic_name",
+                "product_ndc",
+                "unii",
+            ]
+        }
+    )
 
     @cached_property
     def get_raw(self):
@@ -65,11 +76,52 @@ class DrugsFda:
 
     def select_keys(self):
         return [
-            {k: m["openfda"][k] for k in self.cols["openfda"] if k in m["openfda"]}
+            {
+                k: m["openfda"][k]
+                for k in self.cols.default["openfda"]
+                if k in m["openfda"]
+            }
             for m in self.get_raw
             if "openfda" in m
         ]
-        
+
     @cached_property
     def to_df(self):
-        return pd.DataFrame.from_records(self.select_keys())
+        m = [toolz.valmap(get_first_val, m) for m in self.select_keys()]
+        return pd.DataFrame.from_records(m)
+
+    def to_parquet(self, out_path):
+        self.to_df.to_parquet(out_path)
+
+
+@dataclass(frozen=True)
+class Substances:
+    raw: dict = field(
+        default_factory=lambda: open_json_file(
+            "raw/other-substance-0001-of-0001/other-substance-0001-of-0001.json"
+        )
+    )
+    cols: dict = field(default={"codes": ["code_system", "code"]})
+    
+    @cached_property
+    def get_raw(self):
+        return self.raw.default_factory()
+    
+    def select_keys(self):
+        raw = self.get_raw["results"]
+        codes = [m["codes"] for m in raw if "codes" in m]
+        cas_codes = [(list(filter(lambda m: m["code_system"] == "CAS", code))) for code in codes]
+        return cas_codes
+    
+    @cached_property
+    def to_df(self):
+        cas_codes = self.select_keys()
+        dfs = [pd.DataFrame.from_records(c) for c in cas_codes]
+        return pd.concat(dfs)
+        
+
+fda = DrugsFda()
+fda.to_parquet("brick/drugs_fda.parquet")
+
+sub = Substances()
+sub.to_df
