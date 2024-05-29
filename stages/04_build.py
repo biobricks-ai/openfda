17ea847432125629d1e4ffcf2ec6422273614c7c
bhlieberman <bhlieberman93@gmail.com>
2024-05-29 22:26:53 +0000

    unii data added (nsde wip)

--- stages/04_build.py
+++ stages/04_build.py
@@ -1,10 +1,11 @@
-from dataclasses import dataclass, field
+from abc import abstractmethod
 from functools import cached_property
 import json
 import pandas as pd
 from pathlib import Path
 import os
 import toolz
+from typing import Protocol
 
 if not os.path.exists("brick"):
     os.mkdir("brick")
@@ -24,6 +25,9 @@ json_paths = {
     "ndc": "drug-ndc-*",
     "drug_label": "drug-label-*",
     "drugs_fda": "drug-drugsfda-*",
+    "drugs_nsde": "other-nsde-*",
+    "unii": "other-unii-*",
+    "substances": "other-substance-*"
 }
 
 
@@ -32,83 +36,68 @@ def read_json_data(paths):
     for k, v in paths.items():
         file_path = Path(raw_path).rglob(v)
         all = filter(os.path.isfile, file_path)
-        out[k] = list(map(open_json_file, all))
+        out[k] = map(open_json_file, all)
     return out
-
-
-JSON_DATA = read_json_data(json_paths)
 
 
 def get_first_val(vs):
     if len(vs) == 1:
         return vs[0]
     else:
-        return "|".join((val for val in vs))
+        if isinstance(vs, list):
+            return "|".join((val for val in vs))
+        else: return vs
 
 
-@dataclass(frozen=True)
-class DrugsFda:
-    raw = JSON_DATA["drugs_fda"]
-    cols: dict = field(
-        default_factory=lambda: {
-            "openfda": [
-                "brand_name",
-                "application_number",
-                "generic_name",
-                "product_ndc",
-                "unii",
-            ]
-        }
-    )
-
+class OpenFDA(Protocol):
+    raw: dict = read_json_data(json_paths)
+    
     @cached_property
-    def get_raw(self):
-        return list(self.raw)[0]["results"]
-
-    def select_keys(self):
-        return [
-            {k: m["openfda"][k] for k in self.cols["openfda"] if k in m["openfda"]}
-            for m in self.get_raw
-            if "openfda" in m
-        ]
-
-    @cached_property
-    def to_df(self) -> pd.DataFrame:
-        m = [toolz.itemmap(get_first_val, m) for m in self.select_keys()]
-        return pd.DataFrame.from_records(m)
-
-    def to_parquet(self, out_path: os.PathLike):
-        self.to_df.to_parquet(out_path)
-
-
-@dataclass(frozen=True)
-class Substances:
-    raw: dict = field(
-        default_factory=lambda: open_json_file(
-            "raw/other-substance-0001-of-0001/other-substance-0001-of-0001.json"
-        )
-    )
-
-    @cached_property
-    def get_raw(self):
+    def get_raw(self) -> list:
         return self.raw
+    
+    @cached_property
+    def has_multiple(self) -> bool:
+        return toolz.count(self.get_raw) > 1
+    
+    def select_keys(self) -> list:
+        if self.has_multiple:
+            return [m["results"] for m in self.get_raw]
+        else:
+            return self.get_raw[0]["results"]
+    
+    @cached_property
+    @abstractmethod
+    def to_df(self) -> pd.DataFrame:
+        raise NotImplementedError
+    
+    def to_parquet(self, out_path):
+        try:
+            self.to_df.to_parquet(out_path)
+        except Exception as e:
+            print(e)
 
-    def select_keys(self):
-        raw = self.get_raw["results"]
-        codes = [m["codes"] for m in raw if "codes" in m]
-        cas_codes = [
-            (list(filter(lambda m: m["code_system"] == "CAS", code))) for code in codes
-        ]
-        return cas_codes
+
+class DrugsFda(OpenFDA):
 
     @cached_property
-    def to_df(self) -> pd.DataFrame:
-        cas_codes = self.select_keys()
-        dfs = [pd.DataFrame.from_records(c) for c in cas_codes]
-        return pd.concat(dfs)
+    def get_raw(self):
+        return list(super().get_raw["drugs_fda"])
 
-    def to_parquet(self, out_path: os.PathLike):
-        self.to_df.to_parquet(out_path)
+    def __call__(self, out_path):
+        print("Writing drugs_fda data to Parquet.")
+        super().to_parquet(out_path)
+
+
+class Substances(OpenFDA):
+
+    @cached_property
+    def get_raw(self):
+        return list(super().get_raw["substances"])
+
+    def __call__(self, out_path):
+        print("Writing other_substances data to Parquet.")
+        super().to_parquet(out_path)
 
 def cleanup_labels(result):
     out = {}
@@ -124,62 +113,77 @@ def cleanup_labels(result):
             out[k] = v
     return out
 
-@dataclass(frozen=True)
-class DrugsLabel:
-    raw: dict = JSON_DATA["drug_label"]
+class DrugsLabel(OpenFDA):
 
     @cached_property
     def get_raw(self):
-        return list(self.raw)
-
-    def select_keys(self):
-        raw = self.get_raw
-        results = [m["results"] for m in raw]
-        return results
+        return list(super().get_raw["drug_label"])
 
     @cached_property
     def to_df(self):
         ks = [[cleanup_labels(result) for result in lst] for lst in self.select_keys()]
         dfs = [pd.DataFrame.from_records(m) for m in ks]
         return pd.concat(dfs)
-    
-    def to_parquet(self, out_path: os.PathLike):
-        self.to_df.to_parquet(out_path)
         
-@dataclass(frozen=True)
-class NDC:
-    raw: dict = JSON_DATA["ndc"]
+    def __call__(self, out_path):
+        print("Writing labels data to Parquet.")
+        super().to_parquet(out_path)
     
+class NDC(OpenFDA):
+
     @cached_property
     def get_raw(self):
-        return list(self.raw)
+        return list(super().get_raw["ndc"])
 
     def select_keys(self):
-        raw = self.get_raw
-        results = [m["results"] for m in raw]
-        return results
+        return super().select_keys()
     
     @cached_property
     def to_df(self):
-        return pd.concat([pd.DataFrame.from_records(m) for m in self.select_keys()])
+        return pd.DataFrame.from_records(self.select_keys())
     
     def to_parquet(self, out_path: os.PathLike):
-        self.to_df.to_parquet(out_path)
+        super().to_parquet(out_path)
+        
+class UNII(OpenFDA):
     
+    @cached_property
+    def get_raw(self):
+        return list(super().get_raw["unii"])
+    
+    def __call__(self, out_path):
+        print("Writing UNII data to Parquet.")
+        super().to_parquet(out_path)
+        
+class NSDE(OpenFDA):
+    
+    @cached_property
+    def get_raw(self):
+        return list(super().get_raw["drugs_nsde"])
+    
+    def select_keys(self):
+        return super().select_keys()
+    
+    @cached_property
+    def to_df(self):
+        return pd.DataFrame.from_records(self.select_keys())
+    
+    def to_parquet(self, out_path: os.PathLike):
+        super().to_parquet(out_path)
 
 if __name__ == "__main__":
     fda = DrugsFda()
-    print("Writing drugs_fda data to Parquet.")
-    fda.to_parquet("brick/drugs_fda.parquet")
+    fda("brick/drugs_fda.parquet")
     
-    ndc = NDC()
-    print("Writing NDC data to Parquet.")
-    ndc.to_parquet("brick/test_ndc.parquet")
+    # ndc = NDC() not working right now...
+    # print("Writing NDC data to Parquet.")
+    # ndc.to_parquet("brick/test_ndc.parquet")
+    
+    unii = UNII()
+    unii("brick/unii.parquet")
 
     sub = Substances()
-    print("Writing other_substances data to Parquet.")
-    sub.to_parquet("brick/other_substances.parquet")
+    sub("brick/other_substances.parquet")
 
     labels = DrugsLabel()
-    print("Writing labels data to Parquet.")
-    labels.to_parquet("brick/drug_labels.parquet")
+    labels("brick/drug_labels.parquet")
