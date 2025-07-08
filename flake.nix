d34e20812c20487a8e66253a8ee3451b0c125ef7
Ajay <anagyal@insilica.co>
2025-07-08 14:59:54 -0400

    update dependencies

--- flake.nix
+++ flake.nix
@@ -1,9 +1,20 @@
 {
   description = "OpenFDA biobrick";
 
-  inputs = { dev-shell.url = "github:biobricks-ai/dev-shell"; };
-
-  outputs = { self, dev-shell }: {
-    devShells = dev-shell.devShells;
+   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    dev-shell.url = "github:biobricks-ai/dev-shell";
   };
+
+  outputs = { self, nixpkgs, flake-utils, dev-shell }:
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = dev-shell.devShells.${system}.default.overrideAttrs
+          (oldAttrs: {
+            buildInputs = oldAttrs.buildInputs ++ [
+              (python3.withPackages (ps: with ps; [ pandas pyarrow toolz]))
+            ];
+          });
+      });
 }
